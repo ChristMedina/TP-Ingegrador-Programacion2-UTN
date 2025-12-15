18c6dae6d93f00908d2559e3c7ba61357eb8950c
DiegoGRS22 <graus232@gmail.com>
2025-12-14 21:24:05 -0300

    Add files via upload

---
+++ Reportes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include "Reportes.h"
+#include "Venta.h"
+#include "VentaArchivo.h"
+
+using namespace std;
+
+void Reportes::RecaudacionFecha(){
+    VentaArchivo aVenta ("Ventas.dat");
+    int cantRegistros= aVenta.contarRegistros();
+    Venta reg;
+
+    float Recaudacion=0;
+
+    Fecha fecha;
+
+    cout<<"Ingrese la fecha a calcular la recaudacion: ";
+    fecha.cargar();
+
+    for(int i=0;i<cantRegistros;i++){
+        reg=aVenta.LeerR(i);
+        if(reg.getEstado() && reg.getFechaProyeccion()==fecha){
+            Recaudacion+=reg.getImporteTotal();
+        }
+    }
+
+    cout<<"Recaudacion total para la fecha : "<<fecha.mostrar()<<": "<<Recaudacion<<endl;
+
+    system("pause");
+}
---
+++ Reportes.h
@@ -0,0 +1,10 @@
+#ifndef REPORTES_H_INCLUDED
+#define REPORTES_H_INCLUDED
+
+class Reportes{
+    public:
+        void RecaudacionFecha();
+        void RecaudacionAnual();
+};
+
+#endif // REPORTES_H_INCLUDED
